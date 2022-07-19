a8f962ab6ed0a2f37df6ae52b5247a444d87f203
MarcosMunoz99 <marcosmjmg99@gmail.com>
2022-07-19 18:09:56 -0500

    Codigo Actualizado

--- Proyecto_Discord/src/Login.java
+++ Proyecto_Discord/src/Login.java
@@ -11,7 +11,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
